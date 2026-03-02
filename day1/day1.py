f000a2db339faceaa17be3030318b24aba41849e
Dylan Tang <dtang04@uchicago.edu>
2026-03-02 00:13:28 -0600

    Finished aoc day1

--- day1/day1.py
+++ day1/day1.py
@@ -1,3 +1,15 @@
+def get_similarity_score(l_lst, r_lst):
+    ret = 0
+    for l_e in l_lst:
+        count = 0
+        l_e = int(l_e)
+        for r_e in r_lst:
+            r_e = int(r_e)
+            if l_e == r_e:
+                count += 1
+        ret += l_e * count
+    return ret
+
 def main():
     dists = []
     with open("day1.txt", "r") as f:
@@ -14,6 +26,7 @@ def main():
             l_e = int(left_lst[i])
             dists.append(abs(r_e - l_e))
         print(sum(dists)) #Part 1: 1222801
+        print(get_similarity_score(left_lst, right_lst))
 
 if __name__ == "__main__":
     main()
