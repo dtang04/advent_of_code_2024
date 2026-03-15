6e44635d95b02adf81fa85cbc2a36068af2c1bdf
Dylan Tang <dtang04@uchicago.edu>
2026-03-14 21:39:45 -0500

    Finished aoc day7 p2

--- day7/day7.py
+++ day7/day7.py
@@ -12,8 +12,24 @@ def canReachTarget(nums, target, current):
         return True
     return False
 
+def canReachTargetwithConcat(nums, target, current):
+    if len(nums) == 0 and current == target:
+        return True
+    elif len(nums) == 0:
+        return False
+    elif current > target:
+        return False
+    loc = nums[0]
+    mult_s = canReachTargetwithConcat(nums[1:], target, current * loc)
+    add_s = canReachTargetwithConcat(nums[1:], target, current + loc)
+    or_s = canReachTargetwithConcat(nums[1:], target, current * (10 ** len(str(loc))) + loc)
+    if mult_s or add_s or or_s:
+        return True
+    return False
+
 def main():
     part1_count = 0
+    part2_count = 0
     with open("day7.txt", "r") as f:
         for line in f:
             c_pos = line.find(":")
@@ -22,7 +38,11 @@ def main():
             status = canReachTarget(nums[1:], target, nums[0])
             if status:
                 part1_count += target
+            status_2 = canReachTargetwithConcat(nums[1:], target, nums[0])
+            if status_2:
+                part2_count += target
     print(part1_count)
+    print(part2_count)
 
 
 
