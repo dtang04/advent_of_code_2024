b75452165b631a7933cc3c72ceef8c7d6624f55c
Dylan Tang <dtang04@uchicago.edu>
2026-04-21 00:17:48 -0500

    Complete day9

--- day9/day9.py
+++ day9/day9.py
@@ -1,4 +1,9 @@
+freqMap = {}
+
 def buildFileSys(p_input):
+    """
+    Converts the input into the file system.
+    """
     ctr = 0
     isEmpty = False
     out = []
@@ -10,12 +15,16 @@ def buildFileSys(p_input):
         else:
             for i in range(int(p)): # for loop to handle case where id >= 10
                 out.append(str(ctr))
+            freqMap[ctr] = int(p) # build the freqMap for part 2
             isEmpty = True
             ctr += 1
             num_digs += int(p)
     return (out, num_digs)
 
 def organize(files, num_digs):
+    """
+    Part 1
+    """
     end = False
     current_i = len(files) - 1
     while not(end):
@@ -32,6 +41,27 @@ def organize(files, num_digs):
             end = True
     return files
 
+def organize_block(files):
+    current_key = max(freqMap.keys())
+    while current_key >= 0:
+        next_empty = files.index('.')
+        file_loc = files.index(str(current_key)) #find location of beginning of the file
+        flag = False
+        while next_empty < file_loc:
+            ct_contig_empty = 0 #begin counting the contiguous subsequence of empty slots
+            while next_empty + ct_contig_empty < len(files) and files[next_empty + ct_contig_empty] == ".":
+                if ct_contig_empty + 1 == freqMap[current_key]: # if the block is large enough for the given key     
+                    files[next_empty : next_empty + freqMap[current_key]] = [str(current_key)] * freqMap[current_key] # insert the key to fill the contiguous block
+                    files[file_loc : file_loc + freqMap[current_key]] = ['.'] * freqMap[current_key] #replace the old file pos with '.'
+                    flag = True
+                    break
+                ct_contig_empty += 1
+            if flag:
+                break
+            next_empty = files.index('.', next_empty + ct_contig_empty + 1) #jump to the next gap
+        current_key -= 1 
+    return files
+
 def checksum(sorted_files, num_digs):
     chk = 0
     digs = sorted_files[0:num_digs]
@@ -39,6 +69,13 @@ def checksum(sorted_files, num_digs):
         chk += int(c_num) * i
     return chk
 
+def checksum_block(sorted_files):
+    chk = 0
+    for i,c in enumerate(sorted_files):
+        if c == ".":
+            continue
+        chk += int(c) * i
+    return chk
 
 def main():
     p_input = []
@@ -46,9 +83,10 @@ def main():
         for line in f:
             p_input += list(line.strip())
         files, num_digs = buildFileSys(p_input)
-        largest = int(files[-1])
-        sorted_files = organize(files, num_digs)
+        sorted_files = organize(files[:], num_digs)
         print(checksum(sorted_files, num_digs)) #Part 1: 6258319840548
+        sorted_block_files = organize_block(files)
+        print(checksum_block(sorted_block_files))
 
 
 if __name__ == "__main__":
