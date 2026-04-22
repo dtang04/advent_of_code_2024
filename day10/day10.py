c530e9a41edb89ed1c34da539ea0ecc09f831fc7
Dylan Tang <dtang04@uchicago.edu>
2026-04-21 21:15:10 -0500

    Updated

--- day10/day10.py
+++ day10/day10.py
@@ -32,6 +32,24 @@ def ascendBF():
         if pos_x + 1 < len(h_map) and h_map[pos_x+1][pos_y] == level + 1:
             queue.append((pos_x+1, pos_y, start, level+1))
     return ctr
+
+def ascendBFRating():
+    ctr = 0
+    queue = findStarts()
+    while len(queue) > 0:
+        pos_x, pos_y, start, level = queue.pop(0)
+        if level == 9:
+            ctr += 1
+            continue
+        if pos_y - 1 >= 0 and h_map[pos_x][pos_y-1] == level + 1: # Left
+            queue.append((pos_x, pos_y-1, start, level+1))
+        if pos_y + 1 < len(h_map[pos_x]) and h_map[pos_x][pos_y+1] == level + 1: # Right
+            queue.append((pos_x, pos_y+1, start, level+1))
+        if pos_x - 1 >= 0 and h_map[pos_x-1][pos_y] == level + 1: # Up
+            queue.append((pos_x-1, pos_y, start, level+1))
+        if pos_x + 1 < len(h_map) and h_map[pos_x+1][pos_y] == level + 1:
+            queue.append((pos_x+1, pos_y, start, level+1))
+    return ctr
                 
 def main():
     with open("day10.txt", "r") as f:
@@ -42,6 +60,7 @@ def main():
                 row_int.append(int(s))
             h_map.append(row_int)
     print(ascendBF()) # Part 1: 652
+    print(ascendBFRating()) #Part 2: 1432
 
 
 if __name__ == "__main__":
