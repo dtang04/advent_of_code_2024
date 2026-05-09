bf585ca5533a76047134689d0f38ee3967a48037
Dylan Tang <dtang04@uchicago.edu>
2026-05-09 15:58:35 -0700

    Added find_best_paths docstring

--- day16/day16.py
+++ day16/day16.py
@@ -123,6 +123,13 @@ def bfs_heapq(start, end):
     return -1
 
 def find_best_paths(start, end):
+    """
+    For each heapq push, append the next destination of the path onto the trail set.
+
+    Then, for all paths that reach pos == end with the same min_score, add it to the candidate trails list.
+
+    Lastly, aggregate elements with union.
+    """
     global min_score
     global epoch
 
