9f0cae8ff4feb88228d3d923c37c33b7306a33b4
Dylan Tang <dtang04@uchicago.edu>
2026-05-09 15:55:30 -0700

    Complete day16

--- day16/day16.py
+++ day16/day16.py
@@ -2,6 +2,11 @@ import heapq
 
 walls = []
 
+# Part 2 globals
+min_score = None
+min_score_trails = []
+epoch = 0
+
 def bfs(start, end):
     """
     Part 1
@@ -116,7 +121,93 @@ def bfs_heapq(start, end):
             heapq.heappush(pq, (score+1000, (pos_x, pos_y), 'D'))
 
     return -1
+
+def find_best_paths(start, end):
+    global min_score
+    global epoch
+
+    pq = [start] 
+
+    visited = {}
+    while len(pq) > 0:
+        score, pos, d, _, trail = heapq.heappop(pq) # monotonically increasing epoch is to prevent the corner case where score, pos, d are the same
+                                                        # and heapq tries to compare trail, which returns TypeError
+
+        if min_score is not None and score > min_score:
+            continue
+
+        if pos == end and min_score is None: # the first shortest path, may be more shortest paths
+            min_score = score
+            min_score_trails.append(trail)
+            continue
+        elif pos == end and min_score == score:
+            min_score_trails.append(trail)
+            continue
+
+        if (pos, d) in visited and score > visited[(pos, d)]:
+            continue
+        
+        visited[(pos, d)] = score
+        pos_x, pos_y = pos
+
+        if d == 'U':
+            if (pos_x-1, pos_y) not in walls:
+                _trail = trail | {(pos_x-1, pos_y)} # need to create a new copy to pass into func args
+                epoch += 1
+                heapq.heappush(pq, (score+1, (pos_x-1, pos_y), 'U', epoch, _trail))
+            
+            trail = trail.copy()
+            epoch += 1
+            heapq.heappush(pq, (score+1000, (pos_x, pos_y), 'R', epoch, trail))
+            trail = trail.copy()
+            epoch += 1
+            heapq.heappush(pq, (score+1000, (pos_x, pos_y), 'L', epoch, trail))
+        
+        elif d == 'D':
+            if (pos_x+1, pos_y) not in walls:
+                _trail = trail | {(pos_x+1, pos_y)}
+                epoch += 1
+                heapq.heappush(pq, (score+1, (pos_x+1, pos_y), 'D', epoch, _trail))
+                
+            trail = trail.copy()
+            epoch += 1
+            heapq.heappush(pq, (score+1000, (pos_x, pos_y), 'R', epoch, trail))
+            trail = trail.copy()
+            epoch += 1
+            heapq.heappush(pq, (score+1000, (pos_x, pos_y), 'L', epoch, trail))
     
+        elif d == 'L':
+            if (pos_x, pos_y-1) not in walls:
+                _trail = trail | {(pos_x, pos_y-1)}
+                epoch += 1
+                heapq.heappush(pq, (score+1, (pos_x, pos_y-1), 'L', epoch, _trail))
+
+            trail = trail.copy()
+            epoch += 1
+            heapq.heappush(pq, (score+1000, (pos_x, pos_y), 'U', epoch, trail))
+            trail = trail.copy()
+            epoch += 1
+            heapq.heappush(pq, (score+1000, (pos_x, pos_y), 'D', epoch, trail))
+        
+        else:
+            if (pos_x, pos_y+1) not in walls:
+                _trail = trail | {(pos_x, pos_y+1)}
+                epoch += 1
+                heapq.heappush(pq, (score+1, (pos_x, pos_y+1), 'R', epoch, _trail))
+            
+            trail = trail.copy()
+            epoch += 1
+            heapq.heappush(pq, (score+1000, (pos_x, pos_y), 'U', epoch, trail))
+            trail = trail.copy()
+            epoch += 1
+            heapq.heappush(pq, (score+1000, (pos_x, pos_y), 'D', epoch, trail))
+
+    final_nodes = set()
+    for trail_path in min_score_trails:
+        final_nodes = final_nodes.union(trail_path)
+    
+    return len(final_nodes)
+
 def main():
     row = 0
     start = None
@@ -128,15 +219,15 @@ def main():
                 if t == '#':
                     walls.append((row,col))
                 if t == 'S':
-                    start = ((row, col), 'E', 0)
-                    start_pq = (0, (row, col), 'E')
+                    start = ((row, col), 'R', 0)
+                    start_pq = (0, (row, col), 'R')
+                    start_p2 = (0, (row, col), 'R', epoch, {(row, col)})
                 if t == 'E':
                     end = (row, col)
             row += 1
 
     print(bfs_heapq(start_pq, end)) # Part 1: 98416
-
-
+    print(find_best_paths(start_p2, end)) # Part 2: 471
 
 if __name__ == "__main__":
     main()
