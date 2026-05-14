6c64a6de766764090cbde798a0e9c9b4cb416551
Dylan Tang <dtang04@uchicago.edu>
2026-05-13 22:06:41 -0500

    Add day19 day1

---
+++ day19/day19.py
@@ -0,0 +1,41 @@
+from functools import lru_cache
+
+# Globals
+patterns = []
+
+@lru_cache(maxsize=None)
+def isFeasible(c_pattern):
+    out = False
+
+    if len(c_pattern) == 0:
+        return True
+
+    for p in patterns:
+        if c_pattern[:len(p)] == p:
+            out = isFeasible(c_pattern[len(p):])
+
+        if out: # valid combination exists for remaining towels
+            return True
+
+    return False
+            
+def main():
+    global patterns
+
+    ctr = 0
+    is_patterns = True
+
+    with open("day19.txt", 'r') as f:
+        patterns = f.readline().strip().split(", ")
+        f.readline()
+        for l in f:
+            pattern = l.strip()
+            status = isFeasible(pattern)
+    
+            if status:
+                ctr += 1
+    
+    print(ctr)
+
+if __name__ == "__main__":
+    main()
