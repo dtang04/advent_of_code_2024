36d08dab0a488f87c199d145fd39631048f6e5df
Dylan Tang <dtang04@uchicago.edu>
2026-05-13 22:13:16 -0500

    Complete day19

--- day19/day19.py
+++ day19/day19.py
@@ -18,6 +18,20 @@ def isFeasible(c_pattern):
             return True
 
     return False
+
+@lru_cache(maxsize=None)
+def isFeasibleCount(c_pattern):
+    
+    if len(c_pattern) == 0:
+        return 1
+    
+    possible = 0
+
+    for p in patterns:
+        if c_pattern[:len(p)] == p:
+            possible += isFeasibleCount(c_pattern[len(p):])
+    
+    return possible
             
 def main():
     global patterns
@@ -25,17 +39,22 @@ def main():
     ctr = 0
     is_patterns = True
 
+    all_possible = 0
+
     with open("day19.txt", 'r') as f:
         patterns = f.readline().strip().split(", ")
         f.readline()
         for l in f:
             pattern = l.strip()
             status = isFeasible(pattern)
-    
+
+            all_possible += isFeasibleCount(pattern)
+
             if status:
                 ctr += 1
     
     print(ctr) # Part 1: 327
+    print(all_possible) # Part 2: 772696486795255
 
 if __name__ == "__main__":
     main()
