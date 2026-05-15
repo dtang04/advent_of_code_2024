24578efa5f54b22c42b66ae9638d2abf544aad84
Dylan Tang <dtang04@uchicago.edu>
2026-05-15 00:30:04 -0500

    day1 incomplete impl

--- day20/day20.py
+++ day20/day20.py
@@ -1,17 +1,19 @@
 # Globals
-walls = []
 START = None
 END = None
 
 BOARD_LENGTH = 0
 BOARD_WIDTH = 0
 
+walls = []
+cheats = []
+
 def trace():
     """
     Trace the default path.
     """
-    visited = set()
     cur = START
+    visited = {START}
     time_elapsed = 0
     while cur != END:
 
@@ -47,7 +49,98 @@ def trace():
     
     return time_elapsed
 
+
+def traverse():
+    """
+    Solve the race, with cheats.
+    """
+    queue = [(START, 0, 2, False, False, {START})] # cur_pos, time_elapsed, cheat_active_duration, cheat_activated, cheat_used, trail
+                                   # we separate cheat_active_duration and cheat-used
+                                   # cheat of 2 picoseconds means A -> W -> B is allowed
+    
+    visited = set()
+
+    while len(queue) != 0:
+        current = queue.pop()
+
+        cur_pos, time_elapsed, cheat_active_duration, cheat_activated, cheat_used, trail = current
+
+        if cur_pos == END:
+            cheats.append(time_elapsed)
+            continue
+
+        if cur_pos in walls and cheat_active_duration <= 0 and cheat_activated: # no cheat duration left, cheat previously activated, and we're in a wall
+            continue
+        elif cheat_active_duration <= 0 and cheat_activated: # mark the cheat as used when we go back on valid ground
+            cheat_used = True
+            cheat_activated = False
+
+        cur_x, cur_y = cur_pos
+
+        candidate = (cur_x, cur_y-1)
+        if cur_y-1 >= 0 and candidate not in trail:
+            if candidate in walls and cheat_used:
+                continue
+
+            loc_trail = trail.copy()
+            loc_trail.add(candidate)
+
+            if candidate in walls or cheat_activated: # cheat_activated = True can be set if the next move is a wall, or activated in a previous cycle
+                queue.append((candidate, time_elapsed+1, cheat_active_duration-1, True, cheat_used, loc_trail))
+            else:
+                queue.append((candidate, time_elapsed+1, cheat_active_duration, cheat_activated, cheat_used, loc_trail))
         
+        candidate = (cur_x, cur_y+1)
+        if cur_y+1 < BOARD_LENGTH and candidate not in trail:
+            if candidate in walls and cheat_used:
+                continue
+
+            loc_trail = trail.copy()
+            loc_trail.add(candidate)
+
+            if candidate in walls or cheat_activated:
+                queue.append((candidate, time_elapsed+1, cheat_active_duration-1, True, cheat_used, loc_trail))
+            else:
+                queue.append((candidate, time_elapsed+1, cheat_active_duration, cheat_activated, cheat_used, loc_trail))
+        
+        candidate = (cur_x-1, cur_y)
+        if cur_x-1 >= 0 and candidate not in trail:
+
+            if candidate in walls and cheat_used:
+                continue
+
+            loc_trail = trail.copy()
+            loc_trail.add(candidate)
+        
+            if candidate in walls or cheat_activated:
+                queue.append((candidate, time_elapsed+1, cheat_active_duration-1, True, cheat_used, loc_trail))
+            else:
+                queue.append((candidate, time_elapsed+1, cheat_active_duration, cheat_activated, cheat_used, loc_trail))
+
+        candidate = (cur_x+1, cur_y)
+        if cur_x+1 < BOARD_WIDTH and candidate not in trail:
+            if candidate in walls and cheat_used:
+                continue
+
+            loc_trail = trail.copy()
+            loc_trail.add(candidate)
+
+            if candidate in walls or cheat_activated:
+                queue.append((candidate, time_elapsed+1, cheat_active_duration-1, True, cheat_used, loc_trail))
+            else:
+                queue.append((candidate, time_elapsed+1, cheat_active_duration, cheat_activated, cheat_used, loc_trail))
+
+def part1():
+    ctr = 0
+    traverse()
+    d_time = trace()
+
+    for ch_time in cheats:
+        if d_time - ch_time >= 100:
+            ctr += 1
+    
+    return ctr
+
 def main():
     global START, END, BOARD_LENGTH, BOARD_WIDTH
 
@@ -70,7 +163,7 @@ def main():
 
             BOARD_LENGTH += 1
 
-    print(trace())
+    print(part1())
 
 
 if __name__ == "__main__":
